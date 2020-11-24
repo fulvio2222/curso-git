26891565b3757b333cc7838fca1a28f60b32aaf8
Fulvio Ferreira <fulviofcferreira@gmail.com>
2020-11-24 18:39:07 -0300

    Projeto iniciado apresentado Hello World

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Quatro_projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
